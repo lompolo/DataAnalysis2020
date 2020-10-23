a0e2caf9fbab3a56f386ee2fd60c1282694ea620
Tero Lompolo <tero.lompolo@gmail.com>
2020-10-23 13:47:05 +0300

    viikko 1 tehtävä 12

--- part01-e12_distinct_characters/src/distinct_characters.py
+++ part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 def distinct_characters(L):
-    return {}
+    s = dict()
+
+    for p in L:
+        s[p] = len(set(p))
+
+    return s
+
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
 
+
 if __name__ == "__main__":
     main()
+
