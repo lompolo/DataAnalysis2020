3f7fb9e71058a20c1f94d045c8c62d4c4bcd11ed
Tero Lompolo <tero.lompolo@gmail.com>
2020-10-23 17:11:20 +0300

    week 1 ex 14

--- part01-e14_find_matching/src/find_matching.py
+++ part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    r = []
+
+    if not L or not pattern:
+        return r
+
+    for i, w in enumerate(L):
+        if pattern in w:
+            r.append(i)
+    return r
+
 
 def main():
-    pass
+    print(find_matching(["jack", "paul"], ""))
+    print(find_matching(["sensitive", "engine", "rubbish", "comment"], "en"))
+
 
 if __name__ == "__main__":
     main()
+
