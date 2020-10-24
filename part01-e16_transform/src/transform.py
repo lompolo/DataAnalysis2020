3070c693b1097f8768751d72a4e14a10f1e886ff
Tero Lompolo <tero.lompolo@gmail.com>
2020-10-24 19:19:25 +0300

    week 1 ex 16

--- part01-e16_transform/src/transform.py
+++ part01-e16_transform/src/transform.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    return [i * j for i, j in zip(map(int, s1.split()), map(int, s2.split()))]
+
 
 def main():
-    pass
+    print(transform("1 5 3", "2 6 -1"))
+
 
 if __name__ == "__main__":
     main()
+
