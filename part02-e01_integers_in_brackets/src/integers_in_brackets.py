6371acbdcc00d97ac9c9f58ca8a6bed17b9c57a2
Tero Lompolo <tero.lompolo@gmail.com>
2020-10-27 19:14:59 +0200

    week ex1

---
+++ part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+import re
+
+
+def integers_in_brackets(s):
+    return list(map(int, re.findall(r"[[][+]?[\t]?\s?(-?\d+)\s?[]]", s)))
+
+
+def main():
+    print(integers_in_brackets("  afd [asd] [12 ] [a34]  [ -43 ]tt [+12]xxx"))
+
+
+if __name__ == "__main__":
+    main()
+
