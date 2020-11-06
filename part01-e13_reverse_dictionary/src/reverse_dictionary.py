612d6c4894431ae66d4065d61d9a6358bf9d1b9a
Tero Lompolo <tero.lompolo@gmail.com>
2020-11-06 12:46:01 +0200

    week 1 ex 13

--- part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    r = {}
+
+    for k, v in d.items():
+        for i in v:
+            if i in r:
+                r[i].append(k)
+            else:
+                r[i] = [k]
+
+    return r
+
 
 def main():
-    pass
+    d = {'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(d)
+    print(reverse_dictionary(d))
+
 
 if __name__ == "__main__":
     main()
+
